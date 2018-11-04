05e9c6615b316b512f9bf69ce5c110f614a41be5
DawidPL <32899009+DawidPL@users.noreply.github.com>
2018-11-04 16:38:22 +0100

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,9 +1,15 @@
 import requests
 import main
 
-URL = 'https://api.iextrading.com/1.0/stock/'
-PARAMS = {'symbol': ticket, 'constans':'/chart/', 'range': chart_range_eng}
-r = requests.get(url = URL, params=PARAMS)
-
-data = r.json()
-print(i)
+userinput = input('Podaj nazwe spolki:')
+frame = input('Podaj przedział czasowy:')
+def user_search(symbol, rang):
+    URL = "https://api.iextrading.com/1.0/stock/%s/chart/%s" % (symbol, rang)
+    response = requests.get(URL)
+    r = response.json()
+    if response.status_code == requests.codes.ok:
+        print(response.status_code)
+        print(r)
+    else:
+        print('Wystąpił problem z połączenem')
+user_search(userinput, frame)
