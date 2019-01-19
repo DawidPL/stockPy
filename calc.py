14f9f0072254068b8798b8efc9e193a903ea67dc
DawidPL <32899009+DawidPL@users.noreply.github.com>
2019-01-19 22:00:07 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,8 @@
 # calculations
 import math
+
+''' Only for tests '''
+
 input_value = int(input())
 return_percent  = int(input())
 investment_time = int(input())
@@ -12,11 +15,19 @@ commition = float(input('Prowizja transakcji'))
 
 class Calculations():
 
-    # Folding Precentage
-    def foldingPercentage(self, input_value, return_percent, investment_time):
+    def __init__(self, input_value, return_percent, investment_time, shares_amount, bought_price, 
+                 reward_percent, risk_percent, commition):
       self.input_value = input_value
       self.return_percent = return_percent
       self.investment_time = investment_time
+      self.shares_amount = shares_amount
+      self.bought_price = bought_price
+      self.reward_percent = reward_percent
+      self.risk_percent = risk_percent
+      self.commition = commition
+        
+    # Folding Precentage
+    def foldingPercentage(self):
       n = 0
       return_list = []
       while n != investment_time:
@@ -28,15 +39,11 @@ class Calculations():
         print(round(number, 2))
         
     # Calculate cash amount to enter position in specified risk to reward ratio
- def positionCounter(self, shares_amount, bought_price, reward_percent, risk_percent, commition):
-      self.shares_amount = shares_amount
-      self.bought_price = bought_price
-      self.reward_percent = reward_percent
-      self.risk_percent = risk_percent
-      self.commition = commition
+    def positionCounter(self):
       reward = bought_price + (commition / shares_amount) + ((bought_price / 100) * reward_percent)
       lost = bought_price - (commition / shares_amount) - ((bought_price / 100) * risk_percent)
-      print ('Kupujesz {} akcji po cenie {}. Aby zarobić {}% lub ewentualnie stracić {}% podstaw TP na poziomie {} a SL {}'. format(shares_amount, bought_price, reward_percent, risk_percent, reward, lost))
+      print ('Kupujesz {} akcji po cenie {}. Aby zarobić {}% lub ewentualnie stracić {}% podstaw TP na poziomie {} a SL {}'
+             . format(shares_amount, bought_price, reward_percent, risk_percent, reward, lost))
  
  
 calc = Calculations()
