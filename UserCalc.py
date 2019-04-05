0df65f56be76b6ddb816fce8f37cccc253f72b2a
Dawid <dawiduam@gmail.com>
2019-04-05 12:55:23 +0200

    remove Calculations.py, add sl_price_level method to UserCalc

--- UserCalc.py
+++ UserCalc.py
@@ -18,13 +18,20 @@ class UserCalc:
         return folding_percentage_list
 
     @staticmethod
-    def risk_reward_ratio(exchange_price: float, shares_amount: int, expected_profit: float, stoploss_level: float = 0, fee: float = 0) -> float:
-        enter_value = exchange_price * shares_amount
+    def risk_reward_ratio(exchange_entry_price: float, shares_amount: int, expected_profit: float) -> float:
+        enter_value = exchange_entry_price * shares_amount
         wanted_exchange_price: float = 0
         while wanted_exchange_price * shares_amount <= enter_value + expected_profit:
             wanted_exchange_price += 0.01
         return round(wanted_exchange_price, 2)
 
+    @staticmethod
+    def sl_price_level(exchange_entry_price: float, sl_level: float = 0,) -> float:
+        x = (exchange_entry_price / 100) * sl_level
+        y = round(exchange_entry_price - x, 2)
+        return y
+
 
 print(UserCalc.folding_percentage(1000, 10.0, 12))
-print(UserCalc.risk_reward_ratio(1.00, 1000, 100, 0.90, 0.20))
+print(UserCalc.risk_reward_ratio(1.24, 1000, 160))
+print(UserCalc.sl_price_level(10, 3.25))
