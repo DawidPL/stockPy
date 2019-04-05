296ef9d9a910e136a9e88cdd4e043a0b75e18413
Dawid <dawiduam@gmail.com>
2019-04-05 12:15:33 +0200

    add risk_reward_ratio method

--- UserCalc.py
+++ UserCalc.py
@@ -17,5 +17,14 @@ class UserCalc:
             n += 1
         return folding_percentage_list
 
+    @staticmethod
+    def risk_reward_ratio(exchange_price: float, shares_amount: int, expected_profit: float, stoploss_level: float = 0, fee: float = 0) -> float:
+        enter_value = exchange_price * shares_amount
+        wanted_exchange_price: float = 0
+        while wanted_exchange_price * shares_amount <= enter_value + expected_profit:
+            wanted_exchange_price += 0.01
+        return round(wanted_exchange_price, 2)
 
-print(UserCalc.folding_percentage(100, 5.0, 12))
+
+print(UserCalc.folding_percentage(1000, 10.0, 12))
+print(UserCalc.risk_reward_ratio(1.00, 1000, 100, 0.90, 0.20))
